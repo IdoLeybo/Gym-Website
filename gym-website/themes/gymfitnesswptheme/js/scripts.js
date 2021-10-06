2565688c15187fae634d2b7f7296be737bf52696
IdoLeybo <Idol@moveo.co.il>
2021-10-06 09:03:44 +0300

    Added Footer and css to the header and footer

---
+++ gym-website/themes/gymfitnesswptheme/js/scripts.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+    // Make the menu responsive
+    $('#menu-main-navigation').slicknav({
+        appendTo : '.site-header' /* Change the position of the navigation bar */
+    });
+})
--- gym-website/themes/gymfitnesswptheme/page.php
+++ gym-website/themes/gymfitnesswptheme/page.php
@@ -1,7 +1,22 @@
 <?php get_header() ?>
-<?php while(have_posts()): the_post(); ?>
-    <h1><?php the_title(); ?></h1>
+<main class="container page section">
+    <?php while(have_posts()): the_post(); ?>
+        <h1 class="text-center text-primary"><?php the_title(); ?></h1>
 
-    <?php the_content(); ?>
-      
-<?php endwhile; ?>
+<!--        --><?php //the_post_thumbnail() ?><!--    -->
+<!--    //check if an image exist-->
+        <?php
+            if(has_post_thumbnail()):
+                echo "<p> There's an image</p>";
+            else:
+                echo "<p>No image</p>";
+            endif;
+        ?>
+
+        <div class="text-center">
+            <?php the_content(); ?>
+        </div>
+
+    <?php endwhile; ?>
+</main>
+<?php get_footer() ?>
