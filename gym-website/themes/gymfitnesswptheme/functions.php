ed7e92817af73af0ffbc8f932f3eea011b52accd
IdoLeybo <Idol@moveo.co.il>
2021-10-06 11:20:23 +0300

    Added Widget Zone for the side bar Added new Page Template

--- gym-website/themes/gymfitnesswptheme/functions.php
+++ gym-website/themes/gymfitnesswptheme/functions.php
@@ -29,14 +29,33 @@ function gymfitness_scripts() {
     wp_enqueue_script('slicknavjs', get_template_directory_uri() . '/js/jquery.slicknav.min.js', array('jquery'), '1.0.10', true);
 
     wp_enqueue_script('scripts', get_template_directory_uri() . '/js/scripts.js', array('jquery'), '1.0.10', true);
-
-
 }
 
 add_action('wp_enqueue_scripts', 'gymfitness_scripts');
 
 function gymfitness_setup() {
+
+    add_image_size('square', 350, 350, true);
+    add_image_size('portrait', 350, 724, true);
+    add_image_size('box', 400, 375, true);
+    add_image_size('mediumSize', 700, 400, true);
+    add_image_size('blog', 966, 644, true);
+
     add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'gymfitness_setup');
+
+// Creates a Widget Zone
+function gymfitness_widgets() {
+    register_sidebar(array(
+        'name' => 'Sidebar',
+        'id' => 'sidebar',
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'
+    ));
+}
+
+add_action('widgets_init', 'gymfitness_widgets');
--- gym-website/themes/gymfitnesswptheme/page.php
+++ gym-website/themes/gymfitnesswptheme/page.php
@@ -1,22 +1,5 @@
 <?php get_header() ?>
-<main class="container page section">
-    <?php while(have_posts()): the_post(); ?>
-        <h1 class="text-center text-primary"><?php the_title(); ?></h1>
-
-<!--        --><?php //the_post_thumbnail() ?><!--    -->
-<!--    //check if an image exist-->
-        <?php
-            if(has_post_thumbnail()):
-                echo "<p> There's an image</p>";
-            else:
-                echo "<p>No image</p>";
-            endif;
-        ?>
-
-        <div class="text-center">
-            <?php the_content(); ?>
-        </div>
-
-    <?php endwhile; ?>
-</main>
+    <main class="container page section no-sidebar">
+        <?php get_template_part('template-parts/page', 'loop') ?>
+    </main>
 <?php get_footer() ?>
