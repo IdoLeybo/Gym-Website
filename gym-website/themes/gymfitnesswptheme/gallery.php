fc70c042d1a3dfbd2cee4151c4f1315d4719d01b
IdoLeybo <Idol@moveo.co.il>
2021-10-07 14:26:13 +0300

    Added 'lightbox' functionality
    - When you click on an image from the gallery, the image becomes larger and you can move from image to image

--- gym-website/themes/gymfitnesswptheme/gallery.php
+++ gym-website/themes/gymfitnesswptheme/gallery.php
@@ -6,8 +6,25 @@ get_header(); ?>
     <main class="container page section">
         <?php while(have_posts()): the_post(); ?>
             <h1 class="text-center text-primary"><?php the_title(); ?></h1>
+            <?php
+                $gallery = get_post_gallery(get_the_ID(), false); // true - display the gallery, false - just pass the value to the variable
 
-            <?php the_content(); ?>
+                $gallery_images_ids = explode(',', $gallery['ids']);
+            ?>
+            <ul class="gallery-images">
+                <?php
+                    $i = 0;
+                    foreach ($gallery_images_ids as $id):
+                        $size = ($i === 3 || $i === 6) ? 'portrait' : 'square';
+                        $image = wp_get_attachment_image_src($id, 'large');
+                        $imageThumb = wp_get_attachment_image_src($id, $size);
+
+                        ?>
+                        <a href="<?php echo $image[0] ?>" data-lightbox="gallery">
+                            <img src="<?php echo $imageThumb[0] ?>" />
+                        </a>
+                <?php $i++; endforeach; ?>
+            </ul>
         <?php endwhile; ?>
     </main>
 <?php get_footer(); ?>
